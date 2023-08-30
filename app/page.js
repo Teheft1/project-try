5cad2738d08507b37b41c01c3192dd7d2534a3da
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2023-08-30 17:43:31 +0700

    restart all start from scratch

--- app/page.js
+++ app/page.js
@@ -2,59 +2,25 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
-      <div className="z-10 max-w-sm w-full items-center justify-center font-mono text-sm lg:flex ">
-        <h1 className="text-2xl font-extrabold">
-          Welcome To My Webpage
-        </h1>
-      </div>
+      <body>
+        <header>
+          <nav className="fixed justify-between border-b z-10 h-[50px] flex items-center w-full p-5 ">
+            <div className=''>
+              LOGO
+            </div>
+            <div>
+              Another Introduce
+            </div>
+          </nav>
+        </header>
+        <main>
+          <div className="flex items-center justify-center pt-[35%] w-full h-full md:pt-[30%] lg:pt-[22.5%] xl:pt-[15%]">
+            <h1 className="font-sans font-bold text-4xl">
+              Front-end Developer
+            </h1>
+          </div>
+        </main>
+      </body>
 
-      <div className="flex max-">
-        <div>
-          <Image
-              className="relative drop-shadow-md mr-4 "
-              src="/yun.jpg"
-              alt="my photo"
-              width={155}
-              height={50}
-              priority
-            />
-        </div>
-        
-        <div className="">
-          <h1 className='font-mono text-lg'>
-            Hi My Name is Yunus Dhanzky Handitra<br></br>
-            I am student at State University of Surabaya
-          </h1>
-        </div>
-      </div>
-
-      <div className='flex-col '>
-        <h2 className='font-mono text-xl font-bold '
-        >My Skill
-        </h2>
-        <div className='grid'>
-          <Image
-            className='relative'
-            src="/html.svg"
-            alt="html photo"
-            priority
-            width={50}
-            height={50}
-          />
-        </div>
-      </div>
-      <div>
-        <h2 className='font-mono text-xl font-semibold'
-        >This is my work</h2>
-        <ul className=''>
-        <li className=''
-        ><a href='https://daamleon.github.io/sinaupal/'
-        >This Is my work with my friend</a></li>
-        <li></li>
-        <li></li>
-        </ul>
-      </div>
-    </main>
   )
 }
